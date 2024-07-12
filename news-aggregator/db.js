7243acd7e4775a73013c3f75e7e4f2e51891a14d
noa159 <noahaliva123@gmail.com>
2024-07-12 20:04:57 +0300

    create db connection and user scheme

--- news-aggregator/db.js
+++ news-aggregator/db.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log('MongoDB connected...');
     } catch (error) {
-        console.error('MongoDB connection error:', error.message);
+        console.error('MongoDB connection error message:', error.message);
+        console.error('MongoDB connection error stack:', error.stack);
+
         process.exit(1);
     }
 };
